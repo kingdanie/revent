72dffbf910811a9a55bf4937be188b92428aa63f
kingdanie <daniedemola@gmail.com>
2024-03-22 10:23:31 +0100

    import comps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ButtonComponent } from './button/button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, ButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'revent';
+
+  footerLinks: string[] = ["Contact Us", "About Us", "Services", "Our Works"];
+
+
+
 }
